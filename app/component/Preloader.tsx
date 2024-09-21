713e30f5855b51f389b5bb251072b952b381f5b4
Disura Chathwara <disura2005@gmail.com>
2024-09-21 22:11:20 +0530

    update

--- app/component/Preloader.tsx
+++ app/component/Preloader.tsx
@@ -3,7 +3,6 @@ import React from "react";
 const Preloader = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-blue-200 h-screen z-[60] overflow-hidden">
-
       <img src="loader.svg" alt="loader" className="w-20 h-20 lg:w-28 lg:h-28" />
     </div>
   );
