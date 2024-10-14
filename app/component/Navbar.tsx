1a50c78687e5425e5313a2c345a55fb2513c468a
Disura Chathwara <disura2005@gmail.com>
2024-10-14 20:32:47 +0530

    navbar updated

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -115,7 +115,7 @@ const Navbar = ({
                 <li key={idx}>
                   <Link
                     href={navItem.url}
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >
                     {navItem.name}
                   </Link>
@@ -138,7 +138,7 @@ const Navbar = ({
               <li key={idx}>
                 <Link
                   href={navItem.url}
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   {navItem.name}
                 </Link>
